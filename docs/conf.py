19b73e3adf5e6741adc1a1110200d55835268168
Bas Hoonhout <bas@hoonhout.com>
2016-12-07 13:21:40 +0100

    Import package

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,12 @@ import sys
 import os
 import shlex
 import sphinx_rtd_theme
+import oceanwaves
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('../oceanwaves'))
 
 # -- General configuration ------------------------------------------------
 
