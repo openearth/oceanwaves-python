405b950c21ce95cef3bbdadeccda343a2b02c61d
Bas Hoonhout <bas@hoonhout.com>
2017-01-05 16:20:46 +0100

    Added placeholders for Datawell and Wavedroid readers

---
+++ oceanwaves/datawell.py
@@ -0,0 +1,11 @@
+class DatawellReader:
+
+
+    def __init__(self):
+
+        pass
+
+
+    def __call__(self, fpath):
+
+        raise NotImplementedError('Reading of Datawell files is not yet implemented')
---
+++ oceanwaves/wavedroid.py
@@ -0,0 +1,11 @@
+class WaveDroidReader:
+
+
+    def __init__(self):
+
+        pass
+
+
+    def __call__(self, fpath):
+
+        raise NotImplementedError('Reading of WaveDroid files is not yet implemented')
