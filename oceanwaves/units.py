c06d32f1fade10c2427820c4d6041298dd8c3264
Bas Hoonhout <bas.hoonhout@deltares.nl>
2017-10-04 16:27:08 +0200

    Added str as string types to enable unit simplifcation in Python 2.7 again.

--- oceanwaves/units.py
+++ oceanwaves/units.py
@@ -49,7 +49,7 @@ def simplify(units):
         
 
     # only continue in case of string or unicode input
-    if type(units) not in six.string_types + (six.text_type,):
+    if type(units) not in six.string_types + (six.text_type, str):
         return units
     
     # remove spaces around operators (space itself is also a multiplication operator)
